ebf368a67521b9546edb4965874fe14ac00e8111
miekoooo <mocket17@gmail.com>
2023-10-05 19:57:36 -0500

    askdajsd

--- Starter Code/controllers/index.js
+++ Starter Code/controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
-const createUserRoutes = require('./createUserRoutes')
+const createUserRoutes = require('./createUserRoutes');
 
 router.use('/', homeRoutes);
 
