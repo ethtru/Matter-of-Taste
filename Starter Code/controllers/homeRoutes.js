cadc71af282f96e367b2b507300a15ee87dd1361
Eth <ethtru@gmail.com>
2023-10-08 13:24:05 -0400

    adjusted color again

--- Starter Code/controllers/homeRoutes.js
+++ Starter Code/controllers/homeRoutes.js
@@ -6,15 +6,5 @@ router.get("/", async (req, res) => {
   res.render("homepage");
 });
 
-router.get("/categories", async (req, res) => {
-  try {
-    const categories = await Category.findAll({raw: true});
-    console.log(categories);
-    res.render("category", { categories });
-  } catch (err) {
-    console.log(err);
-    res.json(err);
-  }
-});
 
 module.exports = router;
