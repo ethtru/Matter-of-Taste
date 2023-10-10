d6a36395d21794afa18d544b8ce0d70f0f63bc2f
Eth <ethtru@gmail.com>
2023-10-09 23:42:23 -0400

    added express session to server.js as well as login route to homeroutes and express session added to dependencies in package.json

--- Starter Code/server.js
+++ Starter Code/server.js
@@ -1,14 +1,42 @@
 const path = require("path");
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
-// const routes = require("./controllers/api/recipeRoutes");
-const helpers = require("./utils/helpers");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
-const home = require("./controllers/homeRoutes");
+// const routes = require("./controllers/api/recipeRoutes");
+// const helpers = require("./utils/helpers");
+
+// const home = require("./controllers/homeRoutes");
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+// Sets up session and connect to our Sequelize db
+const sess = {
+  secret: "Super secret secret",
+  // Express session will use cookies by default, but we can specify options for those cookies by adding a cookies property to our session options.
+  cookie: {
+    // maxAge sets the maximum age for the cookie to be valid. Here, the cookie (and session) will expire after one hour. The time should be given in milliseconds.
+    maxAge: 60 * 60 * 1000,
+    // httpOnly tells express-session to only store session cookies when the protocol being used to connect to the server is HTTP.
+    httpOnly: true,
+    // secure tells express-session to only initialize session cookies when the protocol being used is HTTPS. Having this set to true, and running a server without encryption will result in the cookies not showing up in your developer console.
+    secure: false,
+    // sameSite tells express-session to only initialize session cookies when the referrer provided by the client matches the domain out server is hosted from.
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitialized: true,
+  // Sets up session store
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 // Create the Handlebars.js engine object with custom helper functions
 const hbs = exphbs.create({ helpers });
@@ -20,10 +48,9 @@ app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-// app.use(routes);
-app.use("/", home);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
