018e0524afcc54ead6e80bda2258b0c70835693d
Eth <ethtru@gmail.com>
2023-10-09 21:50:56 -0400

    added images and login file

--- Starter Code/models/Category.js
+++ Starter Code/models/Category.js
@@ -13,6 +13,10 @@ Category.init(
       type: DataTypes.TEXT,
       allowNull: false,
     },
+    type: {
+      type: DataTypes.ENUM("Dinner", "Breakfast", "Lunch", "Dessert"),
+      allowNull: false,
+    },
   },
   {
     sequelize,
